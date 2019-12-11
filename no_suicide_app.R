c182530e6cb78d539a3fb4675b9dc94bf0a3d180
Anny Chih <annychih@dhcp-128-189-194-204.ubcsecure.wireless.ubc.ca>
2019-12-10 19:41:04 -0800

    filled in layout with text and plots

--- no_suicide_app.R
+++ no_suicide_app.R
@@ -12,7 +12,6 @@ library(gapminder)
 library(ggridges) 
 library(scales)
 library(janitor)
-#library(viridis)
 library(plotly)
 library(ggthemes)
 theme_set(theme_cowplot())
@@ -24,22 +23,13 @@ final_df <- read_csv('data/final_df.csv')
 plot_a_data <- read_csv('data/plot_a_data.csv')
 general_data <- read_csv('data/general_data.csv')
 
-#### CHARTS
-# TAB 0 - PLOT 0A (MAP)
+#### CHART FUNCTIONS
+# TAB (WORLDWIDE OVERVIEW)
 
-# TAB 1 - PLOT 1A (STATIC CONTINENTS)
-plot_1a <- ggplot() +
-    geom_line(data =plot_a_data, aes(x=year, y=suicides_per_100k_pop,color = continent),size = 1)+
-    geom_line(data =general_data, aes(x=year, y=suicides_per_100k_pop,color = Label),size = 1,linetype = "dashed")+
-    ggtitle("Suicide Rate by Continent") +
-    ylab("Suicides per 100k pop") +
-    xlab("Year") +
-    guides(color=guide_legend(title="Continents"))+
-    theme_bw()
-plot_1a <- ggplotly(plot_1a)
-plot_1a
+# TAB 1 - PLOT 1a (STATIC CONTINENTS)
 
-# TAB 1 - PLOT 1B
+
+# TAB 1 - PLOT 1b
 make_plot1b <- function(selected_regions = list('Central America','Western Europe')) {
     regions <- c(selected_regions)
     a <- c('Central America','Western Europe')
@@ -64,7 +54,7 @@ make_plot1b <- function(selected_regions = list('Central America','Western Europ
     return(plot_1b)
 }
 
-# TAB 1 - PLOT 1C
+# TAB 1 - PLOT 1c
 make_plot1c <- function(selected_countries_c = list("Canada", "United States")) {
     countries_c <- c(selected_countries_c)
 
@@ -86,7 +76,7 @@ make_plot1c <- function(selected_countries_c = list("Canada", "United States")) 
     return(country_plot)
 }
 
-# TAB 1 - PLOT 1D
+# TAB 1 - PLOT 1d
 make_plot1d <- function(selected_countries_d = list("Canada", "United States")) {
     countries_d <- c(selected_countries_d)
 
@@ -108,7 +98,7 @@ make_plot1d <- function(selected_countries_d = list("Canada", "United States")) 
     return(dem_plot)
 }
 
-# TAB 2 - PLOT 2A 
+# TAB 2 - PLOT 2a 
 make_plot2a <- function(country_a = 'Any Country', country_b = 'Any Country', year_list = list(1, 1)) {
     # Sets default values
     year_start <- year_list[[1]]
@@ -137,7 +127,7 @@ make_plot2a <- function(country_a = 'Any Country', country_b = 'Any Country', ye
     return(chart_2a)
 }
 
-# TAB 2 - PLOT 2B 
+# TAB 2 - PLOT 2Bb
 make_plot2b <- function(country_a = 'Any Country', country_b = 'Any Country', year_list = list(1,1), demo_selection = list('female : 25-34 years')) {
     # Sets default values
     year_start <- year_list[[1]]
@@ -166,7 +156,7 @@ make_plot2b <- function(country_a = 'Any Country', country_b = 'Any Country', ye
     chart_2b <- ggplotly(chart_2b, tooltip = c("mean_suicides")) %>% config(displayModeBar = FALSE)
     return(chart_2b)
 }
-#### PLOTS
+#### DCC GRAPHS
 graph_1b <- dccGraph(
   id = 'graph_1b',
   figure = make_plot1b(list('Central America','Western Europe'))
@@ -192,78 +182,149 @@ graph_2b <- dccGraph(
   figure = make_plot2b('Canada', 'United States', list(2010,2012), list('male : 15-24 years', 'female : 15-24 years'))
 )
 
-#### SET UP LAYOUT
-app$layout(htmlDiv(list(
-  htmlDiv(className = 'app_body', children = list(
-    dccTabs(id = 'control-tabs', value = 'tabs', children = list(
-      dccTab(
-        label = 'Tab One',
-        value = 'tab one',
-        children = htmlDiv(className = 'control-tab', children = list(
-          htmlP('Written instructions go here.', id = 'text1'),
-          graph_1b
-          #### charts go here
-        ))
-      ),
+#### DROPDOWNS
+regionDD <- dccDropdown(
+  id = 'region_dd',
+  options = map(
+    levels(final_df$sub_region), function(x){
+      list(label=x, value=x)
+    }),
+    value = levels(final_df$sub_region),
+    multi = TRUE
+)
 
-      dccTab(
-        label = 'Tab Two',
-        value = 'tab two',
-        children = htmlDiv(className = 'control-tab', children = list(
-          htmlP('Written instructions go here.', id = 'text2')
-          #### charts go here
-        ))
-      ),
-      
-      dccTab(
-        label = 'Tab Three',
-        value = 'tab three',
-        children = htmlDiv(className = 'control-tab', children = list(
-          htmlP('Written instructions go here.', id = 'text3')
-          #### charts go here
-        ))
-      )
-    ))
-  ))
-)))
+countryDD1 <- dccDropdown(
+  id = 'country_dd_1',
+  options = map(
+    levels(final_df$country), function(x){
+      list(label=x, value=x)
+    }),
+    value = levels(final_df$country),
+    multi = TRUE
+)
 
-# Selection components
+countryDD2a <- dccDropdown(
+  id = 'country_dd_2a',
+  options = map(
+    levels(final_df$country), function(x){
+      list(label=x, value=x)
+    }),
+    value = levels(final_df$country),
+    multi = TRUE
+)
 
-#We can get the years from the dataset to make ticks on the slider
-yearMarks <- map(unique(final_df$year), as.character)
-names(yearMarks) <- unique(final_df$year)
+countryDD2b <- dccDropdown(
+  id = 'country_dd_2b',
+  options = map(
+    levels(final_df$country), function(x){
+      list(label=x, value=x)
+    }),
+    value = levels(final_df$country),
+    multi = TRUE
+)
+
+yearMarks2 <- map(unique(final_df$year), as.character)
+names(yearMarks2) <- unique(final_df$year)
 yearSlider <- dccRangeSlider(
   id = 'year_slider',
-  marks = yearMarks,
+  marks = yearMarks2,
   min = 1986,
   max = 2014,
   step=1,
   value = list(1986, 2014)
 )
 
-continentDropdown <- dccDropdown(
-  id = 'continent_dd',
-  options = map(
-    levels(final_df$continent), function(x){
-      list(label=x, value=x)
-    }),
-  value = levels(final_df$continent), 
+demoCC <- dccDropdown(
+  id = 'demo_cc',
+  value = levels(final_df$demo_group),
   multi = TRUE
 )
 
-# # Storing the labels/values as a tibble means we can use this both 
-# # to create the dropdown and convert colnames -> labels when plotting
-# yaxisKey <- tibble(label = c("GDP Per Capita", "Life Expectancy", "Population"),
-#                    value = c("gdpPercap", "lifeExp", "pop"))
+#### SET UP LAYOUT
+app$layout(htmlDiv(list(
+  htmlH2('Understanding Suicide Rates'),
+  dccMarkdown("
+                The purpose of this app is to help you visualize suicide rates in different locations over time, and see how a variety of different factors (i.e. age, gender, and year) affect these rates.  
+                
+                We have 2 main questions we are trying to answer: 
 
-# yaxisDropdown <- dccDropdown(
-#   id = "filter_dd",
-#   options = map(
-#     1:nrow(yaxisKey), function(i){
-#       list(label=yaxisKey$label[i], value=yaxisKey$value[i])
-#     }),
-#   value = "gdpPercap"
-# )
+                **Tab 1**: How does the suicide rate change over time, and what effect does continent, region, country, age, and gender have on this?  
+                **Tab 2**: How does the suicide rate of one country compare against the suicide rate of another country?   
 
+                Click on the 'Worldwide Overview' tab to begin exploring, and then move on to Tabs 1 and 2 to dive deeper.  
+
+                **If you have thoughts of suicide, please reach out to your local Crisis Centre or Suicide Prevention Hotline.**  
+                **In BC, you can get help by visiting [www.crisiscentre.bc.ca](https://crisiscentre.bc.ca) or by calling 1-800-784-2433 from anywhere in the province.**
+                
+                Source: Suicide Rate Data used in this app is from [Kaggle](https://www.kaggle.com/russellyates88/suicide-rates-overview-1985-to-2016#master.csv)
+                "),
+  htmlDiv(className = 'app_body', children = list(
+    dccTabs(id = 'control-tabs', value = 'tabs', children = list(
+      dccTab(
+        label = 'Overview',
+        value = 'tab value',
+        children = htmlDiv(className = 'control-tab', children = list(
+          htmlP('Written instructions go here.', id = 'text0')
+
+        ))
+      ),
+
+      dccTab(
+        label = 'Tab 1: Continent-Region-Country Analysis',
+        value = 'continent-region-country analysis',
+        children = htmlDiv(className = 'control-tab', children = list(
+          htmlH2('Continent-Region-Country Analysis'),
+
+          # PLOT 1a
+          htmlH3('Suicide Rate by Continent'),
+          dccMarkdown('**Step 1:** This graph shows the average suicide rate over time, by continent.'),
+          graph_1a,
+
+          # PLOT 1b
+          htmlH3('Suicide Rate by Region'),
+          dccMarkdown('**Step 2:** Are there any sub-regions you are specifically interested in looking at?
+                      Select one or more sub-regions (arranged by continent) to view the average suicide rate by year.'),
+          regionDD,
+          graph_1b,
+
+          # PLOT 1c
+          htmlH3('Suicide Rate by Country'),
+          dccMarkdown('**Step 3:** Are there any specific countries you\'d like to look into?
+                      Select one or more countries (arranged by continent) to view the average suicide rate by year.'),
+          countryDD1,
+          graph_1c,
+
+          # PLOT 1d
+          htmlH3('Suicide Rate by Demographic Groups'),
+          dccMarkdown('**Step 4:** This graph will automatically show the average suicide rates for men and women based on the countries you selected previously.'),
+          graph_1d
+        ))
+      ),
+      
+      dccTab(
+        label = 'Tab 3: Two Country Comparison',
+        value = 'two country comparison',
+        children = htmlDiv(className = 'control-tab', children = list(
+          htmlH2('Two Country Comparison'),
+
+          # PLOT 2a
+          dccMarkdown('**Step 1:** Select 2 countries that you would like to compare (1 country per dropdown)'),
+          countryDD2a,
+          countryDD2b,
+          dccMarkdown('**Step 2:** Select a range of years to see the average suicide rate for this time frame.'),
+          yearSlider,
+          graph_2a,
+
+          # PLOT 2b
+          dccMarkdown('**Step 3:** Select one or more groups to see average suicide rates by demographics in each country.'),
+          demoCC,
+          graph_2b
+        ))
+      )
+    ))
+  ))
+)))
+
+#### CALLBACKS
 
 app$run_server()
